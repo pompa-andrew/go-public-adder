d98f7e8d130ca9b848569000ddaae0355d017294
Michał Jazowski <mjazowski@splunk.com>
2024-09-08 16:46:12 +0200

    Comments...

--- public_adder/public-adder.go
+++ public_adder/public-adder.go
@@ -1,7 +1,10 @@
+// Package public_adder it's a package comment!
 package public_adder
 
-// Add # Attention!
-// Add function... adds to number to each other!
+// Add
+//
+// # Attention!
+// Function... adds to number to each other!
 // Jon said I have to add this [link]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
